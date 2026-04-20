fbb92befcd45dce8bb0ae60f4c14365abadbdd5d
SupremeDev021 <supreme.techofc021@gmail.com>
2026-04-20 08:11:15 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,10 +1,13 @@
 const SUPABASE_URL = 'https://ygnphizpnhcsblmwzmjz.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlnbnBoaXpwbmhjc2JsbXd6bXpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY0MzUyNjAsImV4cCI6MjA5MjAxMTI2MH0.hLhpjB5WUDzZX1MRIPVzPVFgq8mcHmnhkhWreAjEFXI';
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+if (typeof window.supabaseClient === 'undefined') {
+    window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+}
+var supabase = window.supabaseClient;
+
 let usuarioAtual = null;
 let perfilAtual = null;
-
 // Verifica se já está logado ao abrir a página
 window.onload = async () => {
     const { data: { session } } = await supabase.auth.getSession();
